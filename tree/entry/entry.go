81677b57b98b72824f516f2277fdc93a553abe74
cheng ma <macheng2025@gmail.com>
2019-11-01 16:27:33 +0800

    通过组合这种方式,扩展struct功能

--- tree/entry/entry.go
+++ tree/entry/entry.go
@@ -5,6 +5,22 @@ import (
 	"learngo/tree"
 )
 
+// 采用组合这种方式扩展类的功能
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
+
 func main() {
 	root := tree.Node{}
 
@@ -36,5 +52,10 @@ func main() {
 	pRoot.Print()
 
 	root.Traverse()
+
+	fmt.Println()
+	node := myTreeNode{&root}
+	node.postOrder()
+	fmt.Println()
 
 }
