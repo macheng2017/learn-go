86656283e280c4c450f92012a6f920d650d46bb1
cheng ma <macheng2025@gmail.com>
2019-11-04 14:50:06 +0800

    go的接口演示 real retriever

--- retriever/main.go
+++ retriever/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"learngo/retriever/mock"
+	real2 "learngo/retriever/real"
 )
 
 type Retriever interface {
@@ -10,10 +11,11 @@ type Retriever interface {
 }
 
 func download(r Retriever) string {
-	return r.Get("www.google.com")
+	return r.Get("http://www.baidu.com")
 }
 
 func main() {
 	// 当前程序不知道Retriever是什么东西,单词故意拼错的
 	fmt.Println(download(mock.Retriever{"this is fack google.com"}))
+	fmt.Println(download(real2.Retriever{}))
 }
