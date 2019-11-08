851f173e769600f043ae847dd27c6de82dc5f497
cheng ma <macheng2025@gmail.com>
2019-11-08 17:49:20 +0800

    示例1. 斐波那契数列改造成文件读写方式

--- functional/fib.go
+++ functional/fib.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
-func fibonacci() func() int {
+func fibonacci() inGen {
 	a, b := 0, 1
 	return func() int {
 		a, b = b, a+b
@@ -16,15 +20,32 @@ func fibonacci() func() int {
 // 1. 在一个函数中返回值是一个函数
 // 2. 返回的函数内部使用了其父函数的一些变量
 
+// 定义一个类型
+type inGen func() int
+
+// 为类型实现接口,方便以后调用 f.Read()
+func (g inGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 10000 {
+		return 0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)      // 将对象转成字符串
+	return strings.NewReader(s).Read(p) // 将字符串写入p 当中
+}
+
+//实现这个接口
+
+func printFileContents(reader io.Reader) {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}
 func main() {
 	f := fibonacci()
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
+
+	printFileContents(f)
 }
