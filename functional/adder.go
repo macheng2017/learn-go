caac9eb0f3e0633ae292d03743eb46a28d2334aa
cheng ma <macheng2025@gmail.com>
2019-11-08 15:11:40 +0800

    函数式编程,闭包,递归2

--- functional/adder.go
+++ functional/adder.go
@@ -11,6 +11,11 @@ func adder() func(int) int {
 }
 
 // 使用一个递归
+// 递归三原则:
+// 1. 递归是调用自己的函数
+// 2. 递归函数中有一个出口条件
+// 3. 递归函数是逐步到达出口条件的
+// 由此可见上面的闭包与递归有本质的区别
 func adder2(arr []int, l int) int {
 	if l == len(arr) {
 		return 0
@@ -21,6 +26,17 @@ func adder2(arr []int, l int) int {
 // user
 func sum(arr []int) int {
 	return adder2(arr, 0)
+}
+
+//  下面的这个"递归" 没有出口条件,太奇怪了
+// 对一些新东西,当自己感觉很不适应,或者感觉奇怪的时候,说明已经达到了自己的认知边界上了
+// ,这时候应该为如果弄懂这个就会延伸自己的认知疆域而感到高兴而不是沮丧
+type iAdder func(int) (int, iAdder)
+
+func adder3(base int) iAdder {
+	return func(v int) (int, iAdder) {
+		return base + v, adder3(base + v)
+	}
 }
 
 func main() {
