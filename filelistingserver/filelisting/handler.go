490eac8bc747ec3a44a76d349eedf88581434583
cheng ma <macheng2025@gmail.com>
2019-11-14 15:08:11 +0800

    将出错信息分为两类,用户看的错误信息,给程序员的错误提示

--- filelistingserver/filelisting/handler.go
+++ filelistingserver/filelisting/handler.go
@@ -1,15 +1,25 @@
 package filelisting
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix = "/list/"
+
 var HandleFileList = func(writer http.ResponseWriter, request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
-	fmt.Printf(path)
+	// 给用户提示输入的url有问题
+	//fmt.Println("request.URL.Path", request.URL.Path)
+	a := strings.Index(request.URL.Path, prefix)
+	if a != 0 {
+		return errors.New("path must start with " + prefix)
+	}
+	path := request.URL.Path[len(prefix):]
+	fmt.Println(path)
 	file, e := os.Open(path)
 	if e != nil {
 
