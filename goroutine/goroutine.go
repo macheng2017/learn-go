33eb6322d82295a86bae1cd852ae174eae538f76
cheng ma <macheng2025@gmail.com>
2019-11-15 18:00:53 +0800

    goroutine 不交出控制权
    goroutine 演示形成闭包,数组下标越界

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -10,14 +10,18 @@ func main() {
 	//fmt.Println("go")
 	var a [10]int
 	for i := 0; i < 10; i++ {
-		go func(i int) {
+		go func() {
+			// 强制go语言只使用一个核,就会出现goroutine不交出控制权的现象
+			//runtime.GOMAXPROCS(1)
 			for {
 				a[i]++
+				// 主动交出控制权
+				//runtime.Gosched()
 			}
-		}(i)
+		}()
 	}
 	//fmt.Println("end")
 	time.Sleep(time.Millisecond)
 	fmt.Println(a)
-
+	//panic: runtime error: index out of range [10] with length 10
 }
