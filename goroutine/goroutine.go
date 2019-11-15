d5a21557818c743a0635d1dcd09da4f963add349
cheng ma <macheng2025@gmail.com>
2019-11-15 18:04:46 +0800

    goroutine 演示形成闭包,数组下标越界

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -5,22 +5,16 @@ import (
 	"time"
 )
 
-// goroutine 会随着主线程结束而结束
 func main() {
-	//fmt.Println("go")
 	var a [10]int
 	for i := 0; i < 10; i++ {
+		// 这里不传值的话就会出现闭包,然后下标越界
 		go func() {
-			// 强制go语言只使用一个核,就会出现goroutine不交出控制权的现象
-			//runtime.GOMAXPROCS(1)
 			for {
 				a[i]++
-				// 主动交出控制权
-				//runtime.Gosched()
 			}
 		}()
 	}
-	//fmt.Println("end")
 	time.Sleep(time.Millisecond)
 	fmt.Println(a)
 	//panic: runtime error: index out of range [10] with length 10
