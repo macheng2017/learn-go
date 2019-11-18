f293b304c4701109e0622383052ee26810d06cc6
cheng ma <macheng2025@gmail.com>
2019-11-18 18:56:57 +0800

    使用select+ default ,使用死循环做一个非阻塞式收数据

--- select/select.go
+++ select/select.go
@@ -5,12 +5,15 @@ import "fmt"
 func main() {
 	var c1, c2 chan int
 	// 使用select+ default ,做一个非阻塞式收数据
-	select {
-	case n := <-c1:
-		fmt.Println(n)
-	case n := <-c2:
-		fmt.Println(n)
-	default:
-		fmt.Println("no value received  ")
+	for {
+		select {
+		case n := <-c1:
+			fmt.Println(n)
+		case n := <-c2:
+			fmt.Println(n)
+		default:
+			fmt.Println("no value received  ")
+		}
 	}
+
 }
