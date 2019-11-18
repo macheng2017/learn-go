d569668da80d789480bd611c5e163fc4b11d9cbe
cheng ma <macheng2025@gmail.com>
2019-11-18 11:10:10 +0800

    channel的示例2

--- channel/channel.go
+++ channel/channel.go
@@ -13,7 +13,7 @@ var worker = func(id int, c chan int) {
 }
 
 func channelDemo() {
-
+	// channel 作为数组
 	var c [10]chan int
 	for i := 0; i < 10; i++ {
 		// 用make给每个nil chan 初始化
@@ -22,10 +22,13 @@ func channelDemo() {
 	}
 
 	// 向channel发数据
+
 	for i := 0; i < 10; i++ {
 		c[i] <- 'a' + i
 	}
-
+	for i := 0; i < 10; i++ {
+		c[i] <- 'A' + i
+	}
 	// 防止main函数提前执行完,结束掉整个程序
 	time.Sleep(time.Millisecond)
 }
@@ -33,13 +36,23 @@ func channelDemo() {
 func main() {
 	channelDemo()
 	//receive value via channel id : 8 value i
-	//receive value via channel id : 0 value a
-	//receive value via channel id : 6 value g
-	//receive value via channel id : 1 value b
+	//receive value via channel id : 3 value d
 	//receive value via channel id : 2 value c
 	//receive value via channel id : 4 value e
-	//receive value via channel id : 3 value d
 	//receive value via channel id : 5 value f
+	//receive value via channel id : 6 value g
 	//receive value via channel id : 7 value h
 	//receive value via channel id : 9 value j
+	//receive value via channel id : 1 value b
+	//receive value via channel id : 0 value a
+	//receive value via channel id : 0 value A
+	//receive value via channel id : 4 value E
+	//receive value via channel id : 1 value B
+	//receive value via channel id : 6 value G
+	//receive value via channel id : 2 value C
+	//receive value via channel id : 5 value F
+	//receive value via channel id : 7 value H
+	//receive value via channel id : 8 value I
+	//receive value via channel id : 9 value J
+	//receive value via channel id : 3 value D
 }
