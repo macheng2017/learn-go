04b203f77803bfff1d4922cec632c30277ea5d81
cheng ma <macheng2025@gmail.com>
2019-11-18 18:55:31 +0800

    使用select+ default ,做一个非阻塞式收数据

---
+++ select/select.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c1, c2 chan int
+	// 使用select+ default ,做一个非阻塞式收数据
+	select {
+	case n := <-c1:
+		fmt.Println(n)
+	case n := <-c2:
+		fmt.Println(n)
+	default:
+		fmt.Println("no value received  ")
+	}
+}
