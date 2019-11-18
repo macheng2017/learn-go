791063cc5ea0188e77a0bc88c75c510212f57213
cheng ma <macheng2025@gmail.com>
2019-11-18 17:42:03 +0800

    尝试将<-worker.done放到最后,却发现有报错,原因是什么?

--- channel/done/done.go
+++ channel/done/done.go
@@ -26,10 +26,11 @@ var doWork = func(id int, c chan int, done chan bool) {
 
 	for n := range c {
 		fmt.Printf("receive value via channel id : %d value %c\n", id, n)
+		done <- true
 	}
 
 	// 为什么这句话放到循环之外就报错了?
-	done <- true
+
 }
 
 func channelDemo() {
@@ -40,34 +41,41 @@ func channelDemo() {
 	}
 
 	// 向channel发数据
-	for i := 0; i < 10; i++ {
-		workers[i].in <- 'a' + i
-		<-workers[i].done
+	for i, worker := range workers {
+		worker.in <- 'a' + i
+
 	}
 
-	for i := 0; i < 10; i++ {
-		workers[i].in <- 'A' + i
-		<-workers[i].done
+	for i, worker := range workers {
+		worker.in <- 'A' + i
+
 	}
-	// 通过添加通信 解决这种让main函数等待goroutine完成
-	//time.Sleep(time.Millisecond)
+
+	// wait for all of them
+	for _, worker := range workers {
+		<-worker.done
+		<-worker.done
+	}
 }
 
 func main() {
 	channelDemo()
 }
 
+//receive value via channel id : 9 value j
+//receive value via channel id : 1 value b
+//receive value via channel id : 3 value d
+//receive value via channel id : 6 value g
+//receive value via channel id : 5 value f
+//receive value via channel id : 8 value i
 //receive value via channel id : 0 value a
+//receive value via channel id : 7 value h
+//receive value via channel id : 2 value c
+//receive value via channel id : 4 value e
 //fatal error: all goroutines are asleep - deadlock!
 //
-//goroutine 1 [chan receive]:
+//goroutine 1 [chan send]:
 //main.channelDemo()
-//	/Users/mac/github/go/src/learngo/channel/done/done.go:45 +0xe0
+//	/Users/mac/github/go/src/learngo/channel/done/done.go:50 +0x16a
 //main.main()
-//	/Users/mac/github/go/src/learngo/channel/done/done.go:57 +0x20
-//
-//goroutine 18 [chan receive]:
-//main.glob..func2(0x0, 0xc00006e060, 0xc00006e0c0)
-//	/Users/mac/github/go/src/learngo/channel/done/done.go:27 +0x10d
-//created by main.glob..func1
-//	/Users/mac/github/go/src/learngo/channel/done/done.go:20 +0x98
+//	/Users/mac/github/go/src/learngo/channel/done/done.go:62 +0x20
