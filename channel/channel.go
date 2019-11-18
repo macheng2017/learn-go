ab2e5bebdf6a0a0ec8faaefbbe71c59118dcc076
cheng ma <macheng2025@gmail.com>
2019-11-18 10:30:33 +0800

    通过channel收发数据

--- channel/channel.go
+++ channel/channel.go
@@ -8,10 +8,21 @@ func channelDemo() {
 	// 在使用select的时候有用.
 	// 使用 make 可以初始化一个channel
 	c := make(chan int)
+	// 如果使用channel发数据没人接收就会deadlock,需要用goroutine接收数据
+
+	go func() {
+		for {
+			n := <-c
+			fmt.Printf("receive value via channel : %d\n", n)
+		}
+	}()
+
+	// 向channel发数据
 	c <- 1
 	c <- 2
-	n := <-c
-	fmt.Println(n)
+	//
+	//n := <-c
+	//fmt.Println(n)
 }
 
 func main() {
