d6034f9e96c8b936d265dd83553f9a441ce5e94c
cheng ma <macheng2025@gmail.com>
2019-11-18 14:02:54 +0800

    buffer channel

--- channel/channel.go
+++ channel/channel.go
@@ -42,26 +42,21 @@ func channelDemo() {
 	time.Sleep(time.Millisecond)
 }
 
-func main() {
-	channelDemo()
-	//receive value via channel id : 8 value i
-	//receive value via channel id : 5 value f
-	//receive value via channel id : 6 value g
-	//receive value via channel id : 1 value b
-	//receive value via channel id : 0 value a
-	//receive value via channel id : 3 value d
-	//receive value via channel id : 2 value c
-	//receive value via channel id : 7 value h
-	//receive value via channel id : 9 value j
-	//receive value via channel id : 4 value e
-	//receive value via channel id : 4 value E
-	//receive value via channel id : 2 value C
-	//receive value via channel id : 0 value A
-	//receive value via channel id : 3 value D
-	//receive value via channel id : 6 value G
-	//receive value via channel id : 7 value H
-	//receive value via channel id : 5 value F
-	//receive value via channel id : 8 value I
-	//receive value via channel id : 9 value J
-	//receive value via channel id : 1 value B
+func bufferedChannel() {
+	// 只发数据没人来收就会报错
+	c := make(chan int)
+	c <- 2
 }
+
+func main() {
+	//channelDemo()
+	bufferedChannel()
+}
+
+//fatal error: all goroutines are asleep - deadlock!
+//
+//goroutine 1 [chan send]:
+//main.bufferedChannel(...)
+//	/Users/mac/github/go/src/learngo/channel/channel.go:46
+//main.main()
+//	/Users/mac/github/go/src/learngo/channel/channel.go:51 +0x50
