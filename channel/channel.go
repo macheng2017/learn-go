95722963dae0bf87b843ef1a3893b2c398651232
cheng ma <macheng2025@gmail.com>
2019-11-18 10:35:18 +0800

    通过channel收发数据,添加注释

--- channel/channel.go
+++ channel/channel.go
@@ -1,16 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func channelDemo() {
-	// var c chan int // c == nil 这样定义也可以但是,没有办法直接使用,
-	// 问题是没法用这种定义方式有什么意义?
-	// 在使用select的时候有用.
-	// 使用 make 可以初始化一个channel
-	c := make(chan int)
-	// 如果使用channel发数据没人接收就会deadlock,需要用goroutine接收数据
 
+	c := make(chan int)
+
+	// 如果使用channel发数据没人接收就会deadlock,需要用goroutine接收数据
 	go func() {
+		// 在goroutine中接收数据
 		for {
 			n := <-c
 			fmt.Printf("receive value via channel : %d\n", n)
@@ -20,13 +21,15 @@ func channelDemo() {
 	// 向channel发数据
 	c <- 1
 	c <- 2
-	//
-	//n := <-c
-	//fmt.Println(n)
+
+	// 防止main函数提前执行完,结束掉整个程序
+	time.Sleep(time.Millisecond)
 }
 
 func main() {
-	// 直接使用的结果是deadlock
 	channelDemo()
-	//	fatal error: all goroutines are asleep - deadlock!
+	//receive value via channel : 1
+	//receive value via channel : 2
+	//
+	//Process finished with exit code 0
 }
