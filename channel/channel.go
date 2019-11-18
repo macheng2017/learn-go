b12ea3f3635ee9c8373fe0b6b3afec22be229432
cheng ma <macheng2025@gmail.com>
2019-11-18 10:54:53 +0800

    channel的示例

--- channel/channel.go
+++ channel/channel.go
@@ -8,19 +8,22 @@ import (
 var worker = func(id int, c chan int) {
 	// 在这里形成了闭包,修改为下面的结构改为非闭包(值传递)
 	for {
-		fmt.Printf("receive value via channel id : %d value %d\n", id, <-c)
+		fmt.Printf("receive value via channel id : %d value %c\n", id, <-c)
 	}
 }
 
 func channelDemo() {
 
-	c := make(chan int)
-
-	go worker(0, c)
+	var c [10]chan int
+	for i := 0; i < 10; i++ {
+		// 初始化10个 goroutine 和10个 channel
+		go worker(i, c[i])
+	}
 
 	// 向channel发数据
-	c <- 1
-	c <- 2
+	for i := 0; i < 10; i++ {
+		c[i] <- 'a' + 1
+	}
 
 	// 防止main函数提前执行完,结束掉整个程序
 	time.Sleep(time.Millisecond)
@@ -28,8 +31,10 @@ func channelDemo() {
 
 func main() {
 	channelDemo()
-	//receive value via channel id : 0 value 1
-	//receive value via channel id : 0 value 2
-	//
-	//Process finished with exit code 0
+	// 执行后发现有个错误,不能向一个 nil chan 发送数据
+	//	goroutine 1 [chan send (nil chan)]:
+	//main.channelDemo()
+	//	/Users/mac/github/go/src/learngo/channel/channel.go:25 +0xaf
+	//main.main()
+	//	/Users/mac/github/go/src/learngo/channel/channel.go:33 +0x20
 }
