e5da7c6486360bc6ec4a4d6432dc19bc1d4a7b2f
cheng ma <macheng2025@gmail.com>
2019-11-19 10:46:15 +0800

    使用 var a chan int 的初始值为nil修改程序

--- select/select.go
+++ select/select.go
@@ -37,26 +37,22 @@ var createWorker = func(id int) chan<- int {
 
 func main() {
 	var c1, c2 = generator(), generator()
-	// 把收过来的n送给work
-	w := createWorker(0)
+	var worker = createWorker(0)
 
+	n := 0
+	hasValue := false
 	for {
-		n := 0
+		var activeWorker chan<- int // 利用这样定义初始值为nil的性质
+		if hasValue {
+			activeWorker = worker
+		}
 		select {
-		// 这样还有问题,就是 47,48行这两个case 收数据会等待一段时间,
-		// 程序会直接执行49行,这时n恒等于0,消费者会一直打印0
 		case n = <-c1:
+			hasValue = true
 		case n = <-c2:
-		case w <- n:
-
+			hasValue = true
+		case activeWorker <- n:
+			hasValue = false
 		}
 	}
 }
-
-//receive value via channel id : 0 value 0
-//receive value via channel id : 0 value 0
-//receive value via channel id : 0 value 0
-//receive value via channel id : 0 value 0
-//receive value via channel id : 0 value 0
-//receive value via channel id : 0 value 0
-//receive value via channel id : 0 value 0
