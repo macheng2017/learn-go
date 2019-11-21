135af68635478d783f55d1285e434d4afb4e58fc
cheng ma <macheng2025@gmail.com>
2019-11-21 17:40:43 +0800

    feat: 自定义client,查看重定向信息

--- http/client.go
+++ http/client.go
@@ -15,7 +15,16 @@ func main() {
 		fmt.Println(e)
 	}
 	request.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
-	resp, err := http.DefaultClient.Do(request)
+	// 自定义一个client,用于查看重定向信息
+	client := http.Client{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			fmt.Println("Redirect: ", req)
+			fmt.Printf("%s\n", via)
+			return nil
+		},
+	}
+	// 将DefaultClient替换成自己定义的client
+	resp, err := client.Do(request)
 	defer resp.Body.Close()
 	if err != nil {
 		fmt.Println(err)
