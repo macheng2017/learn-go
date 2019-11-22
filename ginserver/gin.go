58d95eab7fba372ae0cfc78a34ec1116a29f9f1b
cheng ma <macheng2025@gmail.com>
2019-11-22 18:13:26 +0800

    fixed: 在gin 中添加一个结构化日志信息的中间件

--- ginserver/gin.go
+++ ginserver/gin.go
@@ -8,13 +8,13 @@ import (
 func main() {
 	r := gin.Default()
 	// 添加一个中间件
+	logger, e := zap.NewProduction()
+	if e != nil {
+		panic(e)
+	}
 	r.Use(func(c *gin.Context) {
 		// 结构化log信息
 		// path , log latency, response code
-		logger, e := zap.NewProduction()
-		if e != nil {
-			panic(e)
-		}
 		logger.Info("incoming request", zap.String("path", c.Request.URL.Path))
 		c.Next()
 	})
