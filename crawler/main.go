a26df00e68dfbcdfecb3ae9f0529fe4cd5375d9b
cheng ma <macheng2025@gmail.com>
2019-11-24 17:05:28 +0800

    feat:手动判断网页编码格式

--- crawler/main.go
+++ crawler/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+
+	"golang.org/x/net/html/charset"
+
+	"golang.org/x/text/encoding"
 )
 
 func main() {
@@ -17,11 +25,23 @@ func main() {
 		fmt.Println("Error: status code")
 		return
 	}
+	// 判断网页内容编码格式两种方式
 
-	bytes, err := ioutil.ReadAll(resp.Body)
+	// 1. 手动判断网页编码格式
+	// 添加 go get -v golang.org/x/text/
+	utf8Reader := transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())
+	bytes, err := ioutil.ReadAll(utf8Reader)
+
+	//bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%s", bytes)
+	fmt.Printf("%s\n", bytes)
 
+}
+
+// 2. 自动判断网页内容编码格式
+// 添加 go get  -v golang.org/x/net/html
+func determineEncoding(r io.Reader) encoding.Encoding {
+	charset.DetermineEncoding()
 }
