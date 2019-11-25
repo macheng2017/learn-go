efef0acf4a88095c842b2164e8dd221f25119224
cheng ma <macheng2025@gmail.com>
2019-11-25 11:16:47 +0800

    style2,macheng2025@gmail.com.cn 适应这种情形多个. + 后缀

--- regex/regex.go
+++ regex/regex.go
@@ -7,9 +7,10 @@ import (
 
 func main() {
 	text := `my email is macheng2025@gmail.com
-			email2 macheng2025@gmail.com.cn
+			email2 haha@gmail.com.cn
+	email3 13222@gmail.com.cn@abc.com.cn
 `
-	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+`)
+	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z0-9.]+\.[a-zA-Z0-9]+`)
 
 	s := re.FindAllString(text, -1)
 	fmt.Println(s)
