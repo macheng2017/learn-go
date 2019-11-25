ba1f7e15981f7532741904cbc7687cf0485f831f
cheng ma <macheng2025@gmail.com>
2019-11-25 11:12:19 +0800

    正则表达式,macheng2025@gmail.com.cn 适应这种情形多个. + 后缀

--- regex/regex.go
+++ regex/regex.go
@@ -6,8 +6,10 @@ import (
 )
 
 func main() {
-	text := "my email is macheng2025@gmail.com"
-	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+`)
+	text := `my email is macheng2025@gmail.com
+			email2 macheng2025@gmail.com.cn
+`
+	re := regexp.MustCompile(`[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+`)
 
 	s := re.FindAllString(text, -1)
 	fmt.Println(s)
