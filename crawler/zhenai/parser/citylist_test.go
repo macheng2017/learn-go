c61834a17fc2f7e8d1e9cbb430849ad98fa4f178
cheng ma <macheng2025@gmail.com>
2019-11-26 15:25:14 +0800

    test:这种方式也可以,但是网址内容是在他人的服务器上的, 有两个问题导致每次跑测试不一定能通过: 1.网址会改动 2.内容会改版 3.测试服务器可能没有外网 解决方法: 将文件放在本地

---
+++ crawler/zhenai/parser/citylist_test.go
@@ -0,0 +1,15 @@
+package parser
+
+import (
+	"learngo/crawler/fetcher"
+	"testing"
+)
+
+func TestParseCityList(t *testing.T) {
+	contents, err := fetcher.Fetch("http://www.zhenai.com/zhenghun")
+	if err != nil {
+		panic(err)
+	}
+
+	ParseCityList(contents)
+}
