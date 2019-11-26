53809eb6f62d691172d0e8df9edf5fe735723649
cheng ma <macheng2025@gmail.com>
2019-11-26 16:39:20 +0800

    test:验证470个城市和url数量

--- crawler/zhenai/parser/citylist_test.go
+++ crawler/zhenai/parser/citylist_test.go
@@ -1,15 +1,24 @@
 package parser
 
 import (
-	"learngo/crawler/fetcher"
+	"io/ioutil"
 	"testing"
 )
 
 func TestParseCityList(t *testing.T) {
-	contents, err := fetcher.Fetch("http://www.zhenai.com/zhenghun")
+	//contents, err := fetcher.Fetch("http://www.zhenai.com/zhenghun")
+	contents, err := ioutil.ReadFile("citylist_test_data.html")
+
 	if err != nil {
 		panic(err)
 	}
-
-	ParseCityList(contents)
+	// 测试是否是470个城市的名称和url
+	result := ParseCityList(contents)
+	const resultSize = 470
+	if len(result.Requests) != resultSize {
+		t.Errorf("result should have %d requests; but had %d", resultSize, len(result.Requests))
+	}
+	if len(result.Items) != resultSize {
+		t.Errorf("result should have %d requests; but had %d", resultSize, len(result.Items))
+	}
 }
