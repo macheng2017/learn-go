971562a0c373646ad05a88d6bb3285c310d88a5c
cheng ma <macheng2025@gmail.com>
2019-11-28 10:48:14 +0800

    将Fetcher和Parser的动作提取出来,抽出一个worker概念

--- crawler/engine/engine.go
+++ crawler/engine/engine.go
@@ -24,15 +24,11 @@ func Run(seeds ...Request) {
 		// 2.从队列中一一取出,并使用fetcher拉取并转码
 		r := requests[0]
 		requests = requests[1:]
-		log.Printf("Fetching %s", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+		//
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error searching url %s; %v", r.Url, err)
 			continue
 		}
-
-		// 3.使用相应的解析器解析(main函数初始化engine打包Request传递过来的解析器函数),并将解析出来的
-		parseResult := r.ParserFunc(body)
 
 		// 4.将解析出来并封装好的ParseResult中的Request重新追加到队列中
 		// 将获取到的requests重新放到队列中
@@ -43,4 +39,17 @@ func Run(seeds ...Request) {
 			log.Printf("Got item %v", item)
 		}
 	}
+}
+
+// 将Fetcher和Parser的动作提取出来
+func worker(r Request) (ParseResult, error) {
+	log.Printf("Fetching %s", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error searching url %s; %v", r.Url, err)
+		return ParseResult{}, err
+	}
+
+	// 3.使用相应的解析器解析(main函数初始化engine打包Request传递过来的解析器函数),并将解析出来的
+	return r.ParserFunc(body), nil
 }
