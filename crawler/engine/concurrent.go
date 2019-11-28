3a85e26def0d2133adc72470da64cd59b1eefcaa
cheng ma <macheng2025@gmail.com>
2019-11-28 15:28:17 +0800

    // 1. scheduler从哪里来? 2. scheduler 是什么东西?

--- crawler/engine/concurrent.go
+++ crawler/engine/concurrent.go
@@ -1,13 +1,20 @@
 package engine
 
 type ConcurrentEngine struct {
+	// scheduler在这个结构体中定义了
+	Scheduler
 }
 
-func (ConcurrentEngine) Run(seeds ...Request) {
+// Scheduler 是一个接口
+type Scheduler interface {
+	Submit(r Request)
+}
+
+func (e ConcurrentEngine) Run(seeds ...Request) {
 	// 这里有两个问题:
 	// 1. scheduler从哪里来?
-	// 2. scheduler 是什么东西?
+	// 2. scheduler 是什么东西? scheduler在这个ConcurrentEngine结构体中定义了
 	for _, r := range seeds {
-		scheduler.Submit(r)
+		e.Scheduler.Submit(r)
 	}
 }
