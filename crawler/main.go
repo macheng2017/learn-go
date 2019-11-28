c1723320b4daa701d5d67acc32165ce2a4144338
cheng ma <macheng2025@gmail.com>
2019-11-28 17:10:12 +0800

    为什么重新运行程序卡死?

--- crawler/main.go
+++ crawler/main.go
@@ -8,14 +8,11 @@ import (
 
 func main() {
 	// 包装成SimpleEngine{},将来如果改成ConcurrentEngine{}可以切换过去
-	engine.ConcurrentEngine{
+	e := engine.ConcurrentEngine{
 		Scheduler: &scheduler.SimpleScheduler{},
-	}.Run(engine.Request{
+	}
+	e.Run(engine.Request{
 		Url:        "http://www.zhenai.com/zhenghun",
 		ParserFunc: parser.ParseCityList,
 	})
 }
-
-//# learngo/crawler
-//./main.go:13:3: cannot call pointer method on engine.ConcurrentEngine literal
-//./main.go:13:3: cannot take the address of engine.ConcurrentEngine literal
