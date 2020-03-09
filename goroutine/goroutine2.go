0e8bc3921add8daee34b651afe8c4a9a99b5431d
macheng2017 <gos360@163.com>
2020-03-10 00:19:27 +0800

    feat:非抢占式协程

--- goroutine/goroutine2.go
+++ goroutine/goroutine2.go
@@ -6,17 +6,21 @@ import (
 )
 
 func main() {
-	for i := 0; i < 1000; i++ {
+	var count [10]int
+	for i := 0; i < len(count); i++ {
 		go func(i int) {
 			for {
-				fmt.Printf("Hello from goroutine %d\n", i)
+				count[i]++
+				//runtime.Gosched()
 			}
 		}(i)
 	}
 	time.Sleep(time.Millisecond)
+	fmt.Print(count)
 }
 
 // 需要注意的问题;
 // 1. 匿名函数立即执行函数
 // 2. goroutine 是随着主线程执行完毕而停止
 // 3. goroutine的传值方式闭包
+// 4. goroutine 是非抢占式协程(意味着协程主动(程序员主动控制)交出控制权)
