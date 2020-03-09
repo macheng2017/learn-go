2fe6a727cca4973585e4c6b5143f4691ffe40446
macheng2017 <gos360@163.com>
2020-03-10 00:28:29 +0800

    feat:非抢占式协程 go run -race goroutine2.go 使用-race参数可以发现冲突

--- goroutine/goroutine2.go
+++ goroutine/goroutine2.go
@@ -8,16 +8,22 @@ import (
 func main() {
 	var count [10]int
 	for i := 0; i < len(count); i++ {
-		go func(i int) {
+		// 这里发生了闭包
+		go func() {
 			for {
+				// 这里的 i 值是与函数外层for循环的i值保持一致,当最后一次循环时,i=10这里就会出错
+				// 解决这个问题就是传入一个i让函数内部使用i的副本(值传递)
 				count[i]++
 				//runtime.Gosched()
 			}
-		}(i)
+		}()
 	}
 	time.Sleep(time.Millisecond)
 	fmt.Print(count)
 }
+
+// panic: runtime error: index out of range [10] with length 10
+// go run -race goroutine2.go 使用-race参数可以发现冲突
 
 // 需要注意的问题;
 // 1. 匿名函数立即执行函数
