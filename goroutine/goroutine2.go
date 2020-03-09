9bdea6c2d84ecc74252be503e4048072ee24dd5d
macheng2017 <gos360@163.com>
2020-03-10 00:32:00 +0800

    feat:即便改过来之后也会发现 data race 原因是goroutine 一边在写,一边在读,这个问题可以通过channel来解决

--- goroutine/goroutine2.go
+++ goroutine/goroutine2.go
@@ -8,22 +8,18 @@ import (
 func main() {
 	var count [10]int
 	for i := 0; i < len(count); i++ {
-		// 这里发生了闭包
-		go func() {
+		go func(i int) {
 			for {
-				// 这里的 i 值是与函数外层for循环的i值保持一致,当最后一次循环时,i=10这里就会出错
-				// 解决这个问题就是传入一个i让函数内部使用i的副本(值传递)
 				count[i]++
-				//runtime.Gosched()
 			}
-		}()
+		}(i)
 	}
 	time.Sleep(time.Millisecond)
 	fmt.Print(count)
 }
 
-// panic: runtime error: index out of range [10] with length 10
 // go run -race goroutine2.go 使用-race参数可以发现冲突
+// 即便改过来之后也会发现 data race 原因是goroutine 一边在写,一边在读,这个问题可以通过channel来解决
 
 // 需要注意的问题;
 // 1. 匿名函数立即执行函数
