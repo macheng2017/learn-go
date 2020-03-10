08fb77aad192d616c11558e6caef75531215b72d
macheng2017 <gos360@163.com>
2020-03-10 23:50:58 +0800

    feat:这个错误的原因是,当这个循环完毕之后,消费者对10个channel都发过了一次通信,但是处于没人接受状态,  而这时会进入下个循环,当消费者再次处理完第一个又对channel发一次通信,这时候就报错了,出现deadlock error

--- channel/channel2.go
+++ channel/channel2.go
@@ -22,24 +22,38 @@ func createWorker1(id int) work {
 	return work
 }
 
-// 这样改好之后有个新问题: 打印是顺序执行的,也就是一个goroutine处理完之后通知你完成,
-// 然后再换下一个goroutine,这样每一个goroutine只用了一次,并且会阻塞其他goroutine执行
 func chanDemo() {
 	var works [10]work
 	for i := 0; i < 10; i++ {
 		works[i] = createWorker1(i)
 	}
 
-	for i := 0; i < 10; i++ {
-		works[i].c <- 'a' + i
-		// 这里阻塞了多个goroutine执行,需要排队等待通过,这样就没有了多协程并发执行的优势了
-		<-works[i].done
+	for i, work := range works {
+		work.c <- 'a' + i
+		//这个错误的原因是,当这个循环完毕之后,消费者对10个channel都发过了一次通信,但是处于没人接受状态
+		// 而这时会进入下个循环,当消费者再次处理完第一个又对channel发一次通信,这时候就报错了,出现deadlock error
 	}
 
-	for i := 0; i < 10; i++ {
-		works[i].c <- 'A' + i
-		<-works[i].done
+	for i, work := range works {
+		work.c <- 'A' + i
 	}
+	// wait for all of them
+	for _, worker := range works {
+		<-worker.done
+		<-worker.done
+	}
+
+	// Worker 3 received d
+	//Worker 0 received a
+	//Worker 4 received e
+	//Worker 6 received g
+	//Worker 5 received f
+	//Worker 8 received i
+	//Worker 1 received b
+	//Worker 2 received c
+	//Worker 7 received h
+	//Worker 9 received j
+	//fatal error: all goroutines are asleep - deadlock!
 }
 
 func main() {
