3bd086505e780aeed379d78d49fc4298d2fbdd0d
cheng ma <macheng2025@gmail.com>
2020-03-10 11:38:06 +0800

    feat:为什么把生产者放到消费者前面会导致死锁问题

--- channel/channel2.go
+++ channel/channel2.go
@@ -6,11 +6,20 @@ func chanDemo() {
 	// define the channel 'c' and used make to init
 	c := make(chan int)
 	// channel是goroutine与goroutine之间的通信,main是一个goroutine,需要发送到另外一个goroutine接收
+
+	//for i := 0; i < 100; i++ {
+	//	c <- i
+	//}
+	// 为什么把生产者放到消费者前面会导致死锁问题
 	c <- 1
 	c <- 2
-	//
-	n := <-c
-	fmt.Println(n)
+	go func() {
+		for {
+			n := <-c
+			fmt.Println(n)
+		}
+	}()
+
 }
 
 func main() {
