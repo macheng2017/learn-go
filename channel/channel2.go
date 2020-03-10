300d2b12c12382762f4104cdc7747dc4930b520e
cheng ma <macheng2025@gmail.com>
2020-03-10 11:58:44 +0800

    feat:为什么将生产者放到goroutine当中会没有结果

--- channel/channel2.go
+++ channel/channel2.go
@@ -1,18 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func chanDemo() {
 	// define the channel 'c' and used make to init
 	c := make(chan int)
 	// channel是goroutine与goroutine之间的通信,main是一个goroutine,需要发送到另外一个goroutine接收
 
-	//for i := 0; i < 100; i++ {
-	//	c <- i
-	//}
-	// 为什么把生产者放到消费者前面会导致死锁问题
-	c <- 1
-	c <- 2
 	go func() {
 		for {
 			n := <-c
@@ -20,8 +17,21 @@ func chanDemo() {
 		}
 	}()
 
+	go func() {
+		for {
+			c <- 1
+		}
+	}()
+
+	// 1. 为什么把生产者放到消费者前面会导致死锁问题
+	// 2. 为什么将生产者放到goroutine当中会没有结果
+	// 3. 用-race测试下,测试之后,可以正常打印,猜测问题是,时间太短了两个goroutine没时间启动就随者main goroutine结束了
+	// 4. 解决方式是使用sleep函数,让main函数等一会
+	//c <- 1
+	//c <- 2
 }
 
 func main() {
 	chanDemo()
+	time.Sleep(time.Second)
 } //fatal error: all goroutines are asleep - deadlock!
