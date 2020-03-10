f00ff9e26fe9b556c09a87e0fa87acda80e7c403
macheng2017 <gos360@163.com>
2020-03-11 00:51:28 +0800

    feat:仔细琢磨下这里这样写的好处 通过函数式编程包装下之后将waitGroup抽象掉了放在createWorker中调用时配置

--- channel/channel2.go
+++ channel/channel2.go
@@ -6,19 +6,27 @@ import (
 )
 
 type work struct {
-	id int
-	c  chan int
-	wg *sync.WaitGroup
+	id   int
+	c    chan int
+	done func()
 }
 
 func (work *work) doWorker2() {
 	for n := range work.c {
 		fmt.Printf("Worker %d received %c\n", work.id, n)
-		work.wg.Done()
+		work.done()
 	}
 }
 func createWorker1(id int, wg *sync.WaitGroup) work {
-	work := work{id, make(chan int), wg}
+	work := work{
+		id: id,
+		c:  make(chan int),
+		// 仔细琢磨下这里这样写的好处
+		// 通过函数式编程包装下之后将waitGroup抽象掉了放在createWorker中调用时配置
+
+		done: func() {
+			wg.Done()
+		}}
 	go work.doWorker2()
 	return work
 }
