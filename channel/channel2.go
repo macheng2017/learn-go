310f81458c5a9d52e00670b3805273023bc9cd6c
cheng ma <macheng2025@gmail.com>
2020-03-10 12:03:58 +0800

    feat:生产者消费者 现在是闭包

--- channel/channel2.go
+++ channel/channel2.go
@@ -9,7 +9,7 @@ func chanDemo() {
 	// define the channel 'c' and used make to init
 	c := make(chan int)
 	// channel是goroutine与goroutine之间的通信,main是一个goroutine,需要发送到另外一个goroutine接收
-
+	// 现在是闭包
 	go func() {
 		for {
 			n := <-c
