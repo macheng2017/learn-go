3787ad8b395c15e2f58c136ae6fc581415a63ab4
cheng ma <macheng2025@gmail.com>
2020-03-11 12:17:17 +0800

    feat:问题解决,然后添加消费者函数

--- select/select2.go
+++ select/select2.go
@@ -1,21 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generator1() chan int {
+	out := make(chan int)
+	go func() {
+		i := 0
+		for {
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			out <- i
+			i++
+		}
+	}()
+	return out
+}
 
 func main() {
 
-	var c1, c2 chan int
+	var c1, c2 = generator1(), generator1()
 	//n1 := <-c1
 	//n2 := <-c2
 	// 现在的问题是,我想让这两个channel谁先到谁先收数据,怎么解决?
 
-	select {
-	case n := <-c1:
-		fmt.Println("Received from c1:", n)
-	case n := <-c2:
-		fmt.Println("Received from c2:", n)
-	default:
-		fmt.Println("No value received")
+	for {
+		select {
+		case n := <-c1:
+			fmt.Println("Received from c1:", n)
+		case n := <-c2:
+			fmt.Println("Received from c2:", n)
+			//default:
+			//	fmt.Println("No value received")
+		}
 
 	}
+
 }
