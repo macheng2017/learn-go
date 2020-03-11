9ffb65e68d126139dba951db4e43ebd9215a2398
cheng ma <macheng2025@gmail.com>
2020-03-11 11:43:41 +0800

    feat:使用select调度,非阻塞式获取channel中的数据

--- select/select2.go
+++ select/select2.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	var c1, c2 chan int
-	n1 := <-c1
-	n2 := <-c2
+	//n1 := <-c1
+	//n2 := <-c2
 	// 现在的问题是,我想让这两个channel谁先到谁先收数据,怎么解决?
+
+	select {
+	case n := <-c1:
+		fmt.Println("Received from c1:", n)
+	case n := <-c2:
+		fmt.Println("Received from c2:", n)
+	default:
+		fmt.Println("No value received")
+
+	}
 }
