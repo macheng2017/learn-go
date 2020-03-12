be3bef690a4a796c5393c663a0a026cdc5b968df
cheng ma <macheng2025@gmail.com>
2020-03-12 11:30:06 +0800

    question:在数据读写的时候加上锁

--- basic/atomic/atomic.go
+++ basic/atomic/atomic.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-type atomicInt int
+type atomicInt struct {
+	// go语言中对自定义类型的扩展支持是很好的
+	value int
+	lock  sync.Mutex
+}
 
 // 自定义一个类型atomicInt 并并定义两个方法
 // 1. 递增自己
 // 2. 获取自己的值
 func (a *atomicInt) increment() {
-	*a++
+	// 在数据读写的时候加上锁
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	a.value++
 }
 
 func (a *atomicInt) get() int {
-	return int(*a)
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	return a.value
 }
 
 func main() {
@@ -32,16 +42,3 @@ func main() {
 //虽然是结果是正确的但是,数据读写冲突,需要想办法解决这个问题?
 // 1. 可以用channel + select解决
 // 2. 用传统的同步机制解决
-
-//WARNING: DATA RACE
-//Read at 0x00c0000ae008 by main goroutine:
-//main.main()
-///Users/mac/github/go/src/learngo/basic/atomic/atomic.go:18 +0xc5
-//
-//Previous write at 0x00c0000ae008 by goroutine 7:
-//main.main.func1()
-///Users/mac/github/go/src/learngo/basic/atomic/atomic.go:14 +0x51
-//
-//Goroutine 7 (finished) created at:
-//main.main()
-///Users/mac/github/go/src/learngo/basic/atomic/atomic.go:24 +0xaa
