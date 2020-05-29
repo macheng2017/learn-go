385225b9fbe3fbd5ffb0382223cf6ffb3c13a611
cheng ma <macheng2025@gmail.com>
2020-05-29 16:02:05 +0800

    feat:验证了在for-select 执行每个case不是顺序执行的而是随机且机会是平均的,为什么这么设计?为什么不这么设计?

--- channel2/unblock.go
+++ channel2/unblock.go
@@ -24,15 +24,26 @@ func work1() chan int {
 }
 
 func main() {
-	var c1, c2 = work1(), work1()
+	var c1 = make(chan interface{})
+	close(c1)
+	var c2 = make(chan interface{})
+	close(c2)
+	var c3 = make(chan interface{})
+	close(c3)
+	var c1Count, c2Count, c3Count int
+	for i := 10000000; i >= 0; i-- {
 
-	select {
-	case n := <-c1:
-		fmt.Println(n)
-	case n := <-c2:
-		fmt.Println(n)
-		//default:
-		//	fmt.Println("no value received")
+		select {
+		case <-c1:
+			c1Count++
+		case <-c2:
+			c2Count++
+		case <-c3:
+			c3Count++
+			//default:
+			//	fmt.Println("no value received")
+		}
+
 	}
-
+	fmt.Printf("c1=%v, c2=%v c3=%v", c1Count, c2Count, c3Count)
 }
