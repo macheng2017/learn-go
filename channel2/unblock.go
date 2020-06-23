cd8e95c98abe35c875e4914df1f875515c9dce31
cheng ma <macheng2025@gmail.com>
2020-06-23 19:17:20 +0800

    feat:二刷接口,这样看着比较像函数

--- channel2/unblock.go
+++ channel2/unblock.go
@@ -25,6 +25,7 @@ func generator() chan int {
 
 func work1(w chan interface{}) {
 	for n := range w {
+		time.Sleep(time.Second)
 		fmt.Printf("receive value via channel id: vale =%v \n", n)
 	}
 }
@@ -41,20 +42,33 @@ func main() {
 	var c2 = generator()
 	w1 := createWork1()
 	n := 0
-	hasVale := false
+	var values []int
+	after := time.After(time.Second * 10)
+	tick := time.Tick(time.Second)
 
 	for {
 		var activeWorker chan<- interface{}
-		if hasVale {
+		var activeValue interface{}
+		if len(values) > 0 {
 			activeWorker = w1
+			activeValue = values[0]
 		}
 		select {
 		case n = <-c1:
-			hasVale = true
+			values = append(values, n)
 		case n = <-c2:
-			hasVale = true
-		case activeWorker <- n:
-			hasVale = false
+			values = append(values, n)
+		case <-time.After(time.Millisecond * 800):
+			fmt.Println("timeout")
+
+		case activeWorker <- activeValue:
+			values = values[1:]
+
+		case <-tick:
+			fmt.Printf("Quene =%v %v\n", values, len(values))
+		case <-after:
+			fmt.Println("bye")
+			return
 		}
 	}
 }
--- interface1/main.go
+++ interface1/main.go
@@ -5,7 +5,12 @@ import (
 	"learngo/interface1/infra"
 )
 
+func getRetriever() infra.Retrieve {
+	return infra.Retrieve{}
+}
+
 func main() {
-	retrieve := infra.Retrieve{}
+	// 这样看着比较像函数
+	retrieve := getRetriever()
 	fmt.Println(retrieve.Get("https://www.baidu.com"))
 }
