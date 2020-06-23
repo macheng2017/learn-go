5cc5ddb50ba1d0c316df4d4628ac9baba72bad90
cheng ma <macheng2025@gmail.com>
2020-06-23 19:26:51 +0800

    feat:二刷接口,一个测试团队,需要测试这个方法,但是改动的地方太多了,很不满意

--- interface1/main.go
+++ interface1/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"learngo/interface1/infra"
+	"learngo/interface1/testing"
 )
 
-func getRetriever() infra.Retrieve {
-	return infra.Retrieve{}
+func getRetriever() testing.Retriever {
+	return testing.Retriever{}
 }
 
 func main() {
-	// 把retriever 的类型现式的写出来
-	var retrieve infra.Retrieve = getRetriever()
+	// 一个测试团队,需要测试这个方法,但是改动的地方太多了,很不满意
+	var retrieve testing.Retriever = getRetriever()
 	fmt.Println(retrieve.Get("https://www.baidu.com"))
 }
